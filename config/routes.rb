77cfba48ce86ca7d50783ac50a9158d151744607
SuperSus <sikorskyalexandr08@gmail.com>
2019-04-16 21:44:17 +0300

    create RoomMessage resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :rooms
+  root 'rooms#index'
   devise_for :users
+  resources :room_messages
+  resources :rooms
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
